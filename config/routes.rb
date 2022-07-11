d6ee55910422280a77915d74eae4767b71c1348a
cedric thonney <c.thonney@gmail.com>
2022-07-11 14:09:50 +0400

    define route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   get 'sessions/show'
   get 'welcome/index'
   resources :consultations
-  root to: "pages#home"
+  # sends a get request to the show method in the sessions controller
+  get '/sessions', to: 'sessions#show'
+  # sends a post request to the show method in the sessions controller with a parameter passed from the view
+  post '/sessions', to: 'sessions#show', as: :sessions_show
+  # sends the root to the landig page
+  root 'welcome#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

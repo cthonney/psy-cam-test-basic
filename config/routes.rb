9bbdf794353bfc7cff8944ab0574d43db09dd9a6
cedric thonney <c.thonney@gmail.com>
2022-07-11 14:08:20 +0400

    generates controller welcome+session

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'sessions/show'
+  get 'welcome/index'
   resources :consultations
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html

e32aeeea657de7884614512174e3d16530f30e50
cedric thonney <c.thonney@gmail.com>
2022-07-11 18:02:23 +0400

    clean route

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   resources :consultations
-  # sends a get request to the show method in the sessions controller
-  get '/sessions', to: 'sessions#show'
-  # sends a post request to the show method in the sessions controller with a parameter passed from the view
-  post '/sessions', to: 'sessions#show', as: :sessions_show
-  # sends the root to the landig page
-  root 'welcome#index'
+  root 'consultations#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

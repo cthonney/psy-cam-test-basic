7cd14bdd5c01628555a2c975aa12a5dd65f3b5a7
cedric thonney <c.thonney@gmail.com>
2022-07-11 18:01:35 +0400

    App simplified

--- app/controllers/sessions_controller.rb
+++
@@ -1,6 +0,0 @@
-class SessionsController < ApplicationController
-  def show
-    @consultations = Consultation.all
-    @clicked = params[:consultation]
-  end
-end
--- app/controllers/welcome_controller.rb
+++
@@ -1,5 +0,0 @@
-class WelcomeController < ApplicationController
-  def index
-    @consultations = Consultation.all
-  end
-end
